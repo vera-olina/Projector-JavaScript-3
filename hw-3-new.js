92457b6d2bec512d78f5c648431501e89a70b974
Vira Olina <olina.vera3@gmail.com>
2023-03-23 15:44:04 -0400

    Second commit

--- hw-3-new.js
+++ hw-3-new.js
@@ -1,14 +1,24 @@
 //Task 1
-function durationBetweenDates(date1, date2, word) {
+function durationBetweenDates(date1='31 Jan 2022', date2='1 Jan 2022', word='seconds') {
     const d1 = new Date(date1);
     const d2 = new Date(date2);
-    if (word == 'seconds')
-    return (d2.getTime() - d1.getTime())/1000 + ' ' + word;
-    if (word == 'days')
-    return d2.getDate() - d1.getDate() + ' ' + word;
+    if (word == 'seconds'){
+        return Math.abs((d2.getTime() - d1.getTime())/1000) + ' ' + word;
+    }
+    if (word == 'days'){
+        return Math.abs(d2.getDate() - d1.getDate()) + ' ' + word;
+    }
+    if (word == 'hours'){
+        return Math.abs((d2.getTime() - d1.getTime())/3600000) + ' ' + word;
+    }
+    if (word == 'minutes'){
+        return Math.abs((d2.getTime() - d1.getTime())/60000) + ' ' + word;
+    }
+    console.log()
 }
-console.log(durationBetweenDates('02 Aug 1985', '03 Aug 1985', 'days'))
-  
+console.log(durationBetweenDates());
+console.log(durationBetweenDates('31 Jan 2022', '03 Feb 2021', 'days'));  
+
 //Task 2
 const priceData = { 
     Apples: '23.4', 
@@ -16,22 +26,24 @@ const priceData = {
     oRAngGEs: '48.7584',
 };
   
-function optimizer(data) {
-    return data = Object.entries(priceData).map(([key,value]) => ([key.toLowerCase(), parseFloat(value).toFixed(2)]))
+function optimizer() {
+    return Object.fromEntries(Object.entries(priceData).map(([key,value]) => ([key.toLowerCase(), parseFloat(value).toFixed(2)])))
 };
   
-let updatedPriceData = Object.fromEntries(optimizer(priceData));
+let updatedPriceData = optimizer(priceData);
 console.log(updatedPriceData);
 
 //Task 3
     //Recursive function
 
 function recursiveOddSumTo(n){
-    if (n===1)
-    return 1
-    else if (n%2 == 0)
-    return recursiveOddSumTo(n-1);
-    return n + recursiveOddSumTo(n-1);
+    if (n===1) {
+        return 1
+    } if (n%2 == 0) {
+        return recursiveOddSumTo(n-1);
+    } {
+        return n + recursiveOddSumTo(n-1);
+    }
     }
       
 console.log(recursiveOddSumTo(10))
