19cb923f652beff6f86f94338898b4c2fd931427
Vira Olina <olina.vera3@gmail.com>
2023-04-11 23:20:36 -0400

    Third commit

--- hw-5.2.js
+++ hw-5.2.js
@@ -1,63 +1,27 @@
 'use strict';
 
-const d = document.getElementById('d1');
-d.style.display='flex';
-d.style.justifyContent='center';
-d.style.alignItems='center';
-d.style.flexDirection='column';
-d.style.height='100vh';
-d.style.transition='background 0.2s linear';
+const main = document.getElementById('main');
 
-const cta = document.getElementById('cta');
-cta.style.display='flex';
-cta.style.backgroundColor='180deg, #282B30 0%, #1E2124 100%';
-cta.style.boxShadow='inset 0px 3px 20px rgba(0, 0, 0, 0.15)';
-cta.style.height ='68px';
-cta.style.width='136px';
-cta.style.borderRadius='100px';
-cta.style.position='relative';
+const action_button = document.getElementById('action-button');
 
-const toggle = document.getElementById('toggle');
-toggle.style.backgroundColor='180deg, #ECECEC 20.59%, #C9C9C9 85.35%)';
-toggle.style.height='68px';
-toggle.style.width='68px';
-toggle.style.boxShadow='0px 0px 4px rgba(0, 0, 0, 0.35';
-toggle.style.borderRadius='100px';
-toggle.style.position='absolute';
-toggle.style.transition='transform 0.2s linear';
-toggle.style.display='flex';
-toggle.style.justifyContent='center';
-toggle.style.alignItems='center';
-
-const p1=document.getElementById('p1');
-p1.style.transition='transform 0.2s linear';
-p1.style.color='#31353A';
-
-const p2=document.getElementById('p2');
-p2.style.paddingTop='16px';
-
-const checkbox = document.getElementById('checkbox');
-checkbox.style.display='none';
-checkbox.style.position='absolute';
+const text_message=document.getElementById('text-message');
 
 const checkboxListener = () => {
-    checkbox.addEventListener('click', () =>{
-        if (checkbox.checked) {
-            d.style.backgroundColor='#31353A';
-            toggle.style.transform='translateX(100%)';
-            p1.innerText = 'ON';
-            p1.style.color='white';
-            p2.style.color='white';
-            p2.innerText = 'Last turn off : ' + new Date().toLocaleString();
-        } else {
-            p2.innerText = 'Last turn on : ' + new Date().toLocaleString();
-            p1.innerText = 'OFF';
-            toggle.style.transform='translateX(0)';
-            d.style.backgroundColor='white';
-            p2.style.color='#31353A';
-            p1.style.color='#31353A';
-        }
+    action_button.addEventListener('click', ()=>{
+        action_button.classList.toggle('active');
+        main.classList.toggle('dark-mode');
+        text_message.classList.toggle('active');
+        text_message.innerText= '' + new Date().toLocaleString();
+        if (main.classList.contains('dark-mode')){
+            return localStorage.setItem('mode', 'dark')
+        } localStorage.setItem('mode', 'light')
     })
 }
 
-localStorage.setItem(checkboxListener(), JSON.stringify());
+const saveMode = localStorage.getItem('mode')
+if (saveMode && saveMode === 'dark') {
+    action_button.classList.add('active');
+    main.classList.add('dark-mode');
+}
+
+checkboxListener();
--- hw-5.js
+++ hw-5.js
@@ -2,19 +2,16 @@
 
 const navSelector = document.getElementById('headerTwo');
 const sectionSelector = document.getElementsByTagName('section')[0];
-const listSelector = () =>{
-    const li = document.querySelector('li');
-    if (li.innerHTML = 'Пункт 5'){
-        return console.log(li);
+const givenText = 'Пункт 5';
+const listSelector = [];
+for (let li of document.querySelectorAll('li:nth-child(n)')) {
+    if (li.textContent.includes(givenText)) {
+        listSelector.push(li);
     }
 };
 const classSelector = document.getElementsByClassName('hatredLevelBlock');
 
 console.log(navSelector);
 console.log(sectionSelector);
-listSelector();
+console.log(listSelector);
 console.log(classSelector);
-
-
-
-
