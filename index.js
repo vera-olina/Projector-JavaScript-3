bb8f2d68be88a0101cfe6d86af4bdefe92c23bb9
Vira Olina <olina.vera3@gmail.com>
2023-04-19 11:44:00 -0400

    Recent updates 04/19

--- index.js
+++ index.js
@@ -1,58 +1,44 @@
 `use strict`;
 
 const submit = document.getElementById('submit');
-const output = document.getElementById('output');;
+const output = document.getElementById('output');
 const calcSelector = document.getElementById('option-1');
-const radioButton = document.querySelectorAll('input[type=radio]');
+const presetButton = document.querySelectorAll('#week-button, #month-button');
+const unit = document.getElementById('option-2');
+const d1 = document.getElementById('date-1');
+const d2 = document.getElementById('date-2');
 
-const startApp = () => {
-    submit.addEventListener('click', () => {
-        const date1 = new Date(document.getElementById('date-1').value);
-        const date2 = new Date(document.getElementById('date-2').value);
-        const unit = document.getElementById('option-2');
-        const unitValue = unit.options[unit.selectedIndex].value;
-        const unitText = unit.options[unit.selectedIndex].text.toLowerCase();
-        
-    
-        if(date1.getTime() && date2.getTime()) {
-            const calcDuration = Math.abs(date2.getTime()- date1.getTime());
-            const unitDimension = {
-                1 : calcDuration / (3600000 * 24),
-                2 : calcDuration / 3600000,
-                3 : calcDuration / 60000,
-                4 : calcDuration / 1000
-            }
-        
-            output.innerHTML = 'Difference between the two dates is ' + unitDimension[unitValue] + ' ' + unitText;
-    
-        } 
-        else {
-            output.innerHTML = 'Please select a valid date to start';
+submit.addEventListener('click', calculateSpan);
+
+function calculateSpan() {
+    const date1 = new Date(d1.value);
+    const date2 = new Date(d2.value);
+    const unitValue = unit.options[unit.selectedIndex].value;
+    const unitText = unit.options[unit.selectedIndex].text.toLowerCase();
+
+    presetButton[0].addEventListener('click', getDate2);
+    function getDate2(){
+        if (date1.getTime()) {
+            date2.setDate(date2.getDate() + 7);
+        } else {
+            output.innerHTML = 'Please select Start Date';
         }
-    });
-
-}
-
-function loadResults() {
-    let results;
-
-    if (localStorage.getItem('results') !== null) {
-        results = JSON.parse(localStorage.getItem('results'));
-    } else {
-        results = [];
     }
 
-    results.forEach(function(result) {
-        const li = document.createElement('li');
-        li.className = 'output-item';
-        li.appendChild(document.createTextNode(result));
+    if(date1.getTime() && date2.getTime()) {
+        const calcDuration = Math.abs(date2.getTime()- date1.getTime());
+        const unitDimension = {
+            1 : calcDuration / (3600000 * 24),
+            2 : calcDuration / 3600000,
+            3 : calcDuration / 60000,
+            4 : calcDuration / 1000
+        }
+    
+        output.innerHTML = 'Difference between the two dates is ' + unitDimension[unitValue] + ' ' + unitText;
 
-        outputList.appendChild('li')
-    });
-}
-
-function calcResult(){
-
+    } else {
+        output.innerHTML = 'Please select a valid date to start';
+    }
 }
 
 document.addEventListener('DOMContentLoaded', startApp);
