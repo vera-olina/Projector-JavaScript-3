53a3aacd9a554e245084d3f0814b2a3c71c7de18
Vira Olina <olina.vera3@gmail.com>
2023-04-27 07:26:50 -0400

    Last updates

--- index.js
+++ index.js
@@ -7,81 +7,78 @@ const presetButton = document.querySelectorAll('#week-button, #month-button');
 const unit = document.getElementById('option-2');
 const d1 = document.getElementById('date-1');
 const d2 = document.getElementById('date-2');
+const table = document.getElementById('table');
 
-presetButton[0].addEventListener('click', getDate2Week);
-presetButton[1].addEventListener('click', getDate2Month);
+const startApp = () => {
 
-function getDate2Week() {
-    if (new Date(d1.value).getDate()) {
-        return d2.value = new Date(new Date(d1.value).setDate(new Date(d1.value).getDate() + 7)).toISOString().split('T')[0];
-    } else {
-        output.innerHTML = 'Please select Start Date';
-        // output.classList.add('alert');
-    }
-}
+    presetButton[0].addEventListener('click', getDate2Week);
+    presetButton[1].addEventListener('click', getDate2Month);
 
-function getDate2Month() {
-    if (new Date(d1.value).getDate()) {
-        return d2.value = new Date(new Date(d1.value).setMonth(new Date(d1.value).getMonth() + 1)).toISOString().split('T')[0];
-    } else {
-        output.innerHTML = 'Please select Start Date';
-        // output.classList.add('alert')
-    }
-}
-
-submit.addEventListener('click', calculateSpan);
-
-function calculateSpan() {
-    const date1 = new Date(d1.value);
-    const date2 = new Date(d2.value);
-    const unitValue = unit.options[unit.selectedIndex].value;
-    const unitText = unit.options[unit.selectedIndex].text.toLowerCase();
-
-    if(date1.getTime() && date2.getTime()) {
-        const calcDuration = Math.abs(date2.getTime()- date1.getTime());
-        const unitDimension = {
-            1 : calcDuration / (3600000 * 24),
-            2 : calcDuration / 3600000,
-            3 : calcDuration / 60000,
-            4 : calcDuration / 1000
+    function getDate2Week() {
+        if (new Date(d1.value).getDate()) {
+            return d2.value = new Date(new Date(d1.value).setDate(new Date(d1.value).getDate() + 7)).toISOString().split('T')[0];
+        } else {
+            output.innerHTML = 'Please select Start Date';
+            output.classList.add('alert');
+            setTimeout(()=>{output.classList.remove('alert')}, 1000)
         }
-    
-        output.innerHTML = 'Difference between the two dates is ' + unitDimension[unitValue] + ' ' + unitText;
+    }
 
-    } else {
-        output.innerHTML = 'Please select a valid date to start';
-        // output.classList.add('alert')
+    function getDate2Month() {
+        if (new Date(d1.value).getDate()) {
+            return d2.value = new Date(new Date(d1.value).setMonth(new Date(d1.value).getMonth() + 1)).toISOString().split('T')[0];
+        } else {
+            output.innerHTML = 'Please select Start Date';
+            output.classList.add('alert');
+            setTimeout(()=>{output.classList.remove('alert')}, 1000)
+        }
+    }
+
+    submit.addEventListener('click', calculateSpan);
+
+    function calculateSpan() {
+        const date1 = new Date(d1.value);
+        const date2 = new Date(d2.value);
+        const unitValue = unit.options[unit.selectedIndex].value;
+        const unitText = unit.options[unit.selectedIndex].text.toLowerCase();
+
+        if(date1.getTime() && date2.getTime()) {
+            const calcDuration = Math.abs(date2.getTime()- date1.getTime());
+            const unitDimension = {
+                1 : calcDuration / (3600000 * 24),
+                2 : calcDuration / 3600000,
+                3 : calcDuration / 60000,
+                4 : calcDuration / 1000
+            }
+        
+            output.innerHTML = 'Difference between the two dates is ' + unitDimension[unitValue] + ' ' + unitText;
+
+            const rowCapacity = table.rows.length;
+            if (rowCapacity > 10) return //table.deleteRow(-1);
+
+            const row = table.insertRow(1);
+            const cell1 = row.insertCell(0);
+            const cell2 = row.insertCell(1);
+            const cell3 = row.insertCell(2);
+            const cell4 = row.insertCell(3)
+            cell1.innerHTML = date1.toLocaleDateString();
+            cell2.innerHTML = date2.toLocaleDateString();
+            cell3.innerHTML = unitDimension[unitValue] + ' ' + unitText;
+            cell4.innerHTML = `<button class="delete-button"></button>`;
+            cell4.classList.add('delete');
+            table.style.display = 'table';
+
+        } else {
+            output.innerHTML = 'Please select a valid date to start';
+            output.classList.add('alert');
+            setTimeout(()=>{output.classList.remove('alert')}, 1000)
+        }
+    }
+
+    calcSelector.addEventListener('click', determineDays);
+
+    function determineDays() {
     }
 }
 
-calcSelector.addEventListener('click', determineDays);
-
-// function determineDays() {
-//     const calcSelectorValue = calcSelector.options[calcSelector.selectedIndex].value;
-//     const nWeekdays = Math.floor( (date2.getDay()+calcDuration) / 7 )
-//     determine = {
-//         1:
-//         2:
-//         3: 2*nWeekdays + (date2.getDay()==0) - (date1.getDay()==6);
-//     }
-// }
-
-// function loadResults() {
-//     let results;
-
-//     if (localStorage.getItem('results') !== null) {
-//         results = JSON.parse(localStorage.getItem('results'));
-//     } else {
-//         results = [];
-//     }
-
-//     results.forEach(function(result) {
-//         const li = document.createElement('li');
-//         li.className = 'output-item';
-//         li.appendChild(document.createTextNode(result));
-
-//         outputList.appendChild('li')
-//     });
-// }
-
-// document.addEventListener('DOMContentLoaded', startApp);
+document.addEventListener('DOMContentLoaded', startApp);
