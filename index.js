f5112af4fb52d11ba7cd8087eb34fd535b72735e
Vira Olina <olina.vera3@gmail.com>
2023-05-25 10:01:08 -0400

    Recent commit

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ const unit = document.getElementById('option-2');
 const d1 = document.getElementById('date-1');
 const d2 = document.getElementById('date-2');
 const table = document.getElementById('table');
-const deleteButton = document.getElementsByClassName('delete-button');
 
 const startApp = () => {
-
     presetButton[0].addEventListener('click', getDate2Week);
     presetButton[1].addEventListener('click', getDate2Month);
 
@@ -35,6 +33,51 @@ const startApp = () => {
         }
     }
 
+    calcSelector.addEventListener('change', determineDays);
+
+    function determineDays() {
+        const calcSelectorValue = calcSelector.options[calcSelector.selectedIndex].value;
+
+        function weekdaysCounter(){
+            const date1 = new Date(d1.value);
+            const date2 = new Date(d2.value);
+            let counter = 0;
+            while (date1 < date2) {
+                const dayOfWeek = date1.getDay();
+                if (dayOfWeek !== 0 && dayOfWeek !== 6){
+                    counter++;
+                }
+                date1.setDate(date1.getDate() + 1);
+            }
+            return counter;
+        }
+
+        function weekendsCounter() {
+            const date1 = new Date(d1.value);
+            const date2 = new Date(d2.value);
+            let counter = 0;
+            while (date1 < date2) {
+                const dayOfWeek = date1.getDay();
+                if (dayOfWeek == 0 || dayOfWeek == 6){
+                    counter++;
+                }
+                date1.setDate(date1.getDate() + 1);
+            }
+            return counter;
+        }
+
+        const determineDaysFilter = {
+            1 : 0,
+            2 : weekendsCounter(),
+            3 : weekdaysCounter(),
+        }
+
+        if (typeof(determineDaysFilter[calcSelectorValue]) == 'undefined') {
+            return 0;
+        }
+        return determineDaysFilter[calcSelectorValue] * 86400000;
+    }
+
     submit.addEventListener('click', calculateSpan);
 
     function calculateSpan() {
@@ -43,8 +86,8 @@ const startApp = () => {
         const unitValue = unit.options[unit.selectedIndex].value;
         const unitText = unit.options[unit.selectedIndex].text.toLowerCase();
 
-        if(date1.getTime() && date2.getTime()) {
-            const calcDuration = Math.abs(date2.getTime()- date1.getTime());
+        if(date1.getTime() && date2.getTime() && date1.getTime()<date2.getTime()) {
+            const calcDuration = Math.abs(date2.getTime()- date1.getTime() - determineDays());
             const unitDimension = {
                 1 : calcDuration / (3600000 * 24),
                 2 : calcDuration / 3600000,
@@ -58,40 +101,34 @@ const startApp = () => {
             const cell1 = row.insertCell(0);
             const cell2 = row.insertCell(1);
             const cell3 = row.insertCell(2);
-            const cell4 = row.insertCell(3)
             cell1.innerHTML = date1.toLocaleDateString();
             cell2.innerHTML = date2.toLocaleDateString();
             cell3.innerHTML = unitDimension[unitValue] + ' ' + unitText;
-            cell4.innerHTML = `<button class="delete-button"></button>`;
-            cell4.classList.add('delete');
             table.style.display = 'table';
 
+            const tableData = { cell1, cell2, cell3 };
+            let dataLS = [];
+            dataLS.push(tableData);
+
+            const lsListenner = (dataLS) => {
+                localStorage.setItem('tableData', JSON.stringify(dataLS));
+                localStorage.getItem('tableData');
+                JSON.parse(localStorage.getItem('tableData'));
+            }
+
+            lsListenner(dataLS);
+
+            const rowCapacity = document.querySelector('tbody').rows.length;
+            if (rowCapacity > 11) {
+                return document.querySelector('tbody').deleteRow(-1)
+            }
+            
         } else {
             output.innerHTML = 'Please select a valid date to start';
             output.classList.add('alert');
             setTimeout(()=>{output.classList.remove('alert')}, 900)
         }
     }
-
-    function rowCapacityFunc() {
-        const rowCapacity = table.rows.length;
-        if (rowCapacity > 2) return //table.deleteRow(-1);
-    }
-
-    // deleteButton.addEventListener('click', deleteRow);
-
-    // function deleteRow() {
-    //     return
-    // }
-
-    calcSelector.addEventListener('click', determineDays);
-
-    function determineDays() {
-        // var ndays = 1 + Math.round((d1.getTime()-d0.getTime())/(24*3600*1000));
-        // var nsaturdays = Math.floor( (d0.getDay()+ndays) / 7 );
-        // return 2*nsaturdays + (d0.getDay()==0) - (d1.getDay()==6);
-    }
-
 }
 
 document.addEventListener('DOMContentLoaded', startApp);
